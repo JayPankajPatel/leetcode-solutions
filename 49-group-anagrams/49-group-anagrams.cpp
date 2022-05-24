a25339ad897cf5f0ed8444270d6d63831904d658
Jay Patel <37052409+JayPankajPatel@users.noreply.github.com>
2022-05-24 15:16:13 -0700

    Time: 59 ms (38.16%), Space: 19.7 MB (70.74%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,35 +2,25 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         using namespace std; 
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+        
         unordered_map<string,vector<string>> my_map;
         
-        char char_count[27] = {};
-        char_count[26] = 0;
-       
-        for(string & curr_str : strs) {
-            std::fill_n(char_count, 26, 1);
-            
-            for(int j = 0; j < curr_str.size(); j++) {
-                char_count[curr_str[j] - 'a']++;
-                for(auto x : char_count)
-                {
-                    cout << x << " "; 
-                }
-                cout << endl;
-            }
-
-            string to_hash(char_count);
-            
-            my_map[to_hash].push_back(curr_str); 
+        for(int i = 0; i < strs.size(); i++)
+        {
+            string s = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            my_map[strs[i]].push_back(s); 
         }
         
         vector<vector<string>> ans;
         
-        for( auto&[key,value] : my_map) {
+        for( auto&[key,value] : my_map)
+        {
             ans.push_back(value);
         }
+        
+    
+    
         
         return ans;
     }
