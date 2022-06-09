ef5d31cb878d210b7f21cfe237071c414000fcee
Jay Patel <37052409+JayPankajPatel@users.noreply.github.com>
2022-06-09 01:56:09 -0400

    Time: 175 ms (18.15%), Space: 14.2 MB (67.50%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -4,14 +4,12 @@ class Solution:
         merge = nums1 + nums2
         
         merge.sort()
-        print(merge)
         if(len(merge) % 2 != 0):
             mid = len(merge) // 2
             return merge[mid]
         else:
             
-            mid = len(merge) // 2 -1 
-            print((merge[mid] + merge[mid + 1]))
+            mid = len(merge) // 2 - 1 
             return (merge[mid] + merge[mid + 1]) / 2  
         
         
