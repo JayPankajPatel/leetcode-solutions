75619032f7b9e38b9380431dd1c5e380b9198c72
Jay Patel <37052409+JayPankajPatel@users.noreply.github.com>
2022-06-11 15:52:39 -0400

    Time: 77 ms (29.58%), Space: 16.3 MB (83.76%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -5,21 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root: TreeNode) -> int:
-        if(not root):
-            return 0
-        left = self.dfs(root.left) + 1
-        right = self.dfs(root.right) + 1
-        
-        return max(left, right)
-        
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if(not root):
-            return 0
+        res = [0]
         
-        op1 = self.dfs(root.left) + self.dfs(root.right)
-        op2 = self.diameterOfBinaryTree(root.left)
-        op3 = self.diameterOfBinaryTree(root.right)
+        def dfs(root):
+            if(not root):
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            res[0] = max(res[0], left + right)
+            
+            return (1 + max(left, right))
         
-        
-        return max(op1, max(op2,op3))
+        dfs(root)
+        return res[0]
+                            
