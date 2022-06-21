6564468ccf3ca53e5e9fc01f52c626688d3fc6d8
Jay Patel <37052409+JayPankajPatel@users.noreply.github.com>
2022-06-20 21:48:37 -0400

    Time: 39 ms (78.79%), Space: 26.9 MB (26.58%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        std::vector<int> res;
+        
+        int left = 0; 
+        int right = nums.size() - 1;
+        
+        while(left <= right) {
+            if(std::abs(nums[left]) > std::abs(nums[right])) {
+                res.emplace_back(nums[left] * nums[left]);
+                left++; 
+            }
+            
+            else {
+                res.emplace_back(nums[right] * nums[right]);
+                right--;
+            }
+        }
+        
+        std::reverse(res.begin(), res.end());
+        
+        return res; 
+    }
+};
