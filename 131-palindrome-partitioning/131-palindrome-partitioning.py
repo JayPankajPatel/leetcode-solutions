ecf0a2eb26cf595ee8f0d8d40bd4205ed4e8f867
Jay Patel <37052409+JayPankajPatel@users.noreply.github.com>
2022-06-27 01:11:27 -0400

    Time: 1140 ms (22.42%), Space: 30.1 MB (63.19%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        
+        def is_palindrome(word: str) -> bool:
+            return word == word[::-1]
+        
+        def dfs(start: int, route: List[str]):
+            if start == len(s):
+                res.append(route[:])
+                return
+            
+            for i in range(start, len(s)):
+                substring = s[start : i + 1]
+                if is_palindrome(substring):
+                    route.append(substring)
+                    dfs(i + 1, route)
+                    route.pop()
+        
+        
+        dfs(0, [])
+        
+        return res
+        
