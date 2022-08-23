d303c7d23f98092c674fb989733c8b080e386d47
Jay Patel <37052409+JayPankajPatel@users.noreply.github.com>
2022-08-22 20:45:00 -0400

    Time: 72 ms (7.47%), Space: 13.8 MB (98.20%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        from collections import deque
+        
+        check = {
+            ']':'[',
+            '}': '{', 
+            ')': '('
+        }
+        stack = deque()
+        
+        for x in s:
+            if stack and x in check and stack[-1] == check[x]:
+                stack.pop()
+                continue
+            stack.append(x)
+            
+        return len(stack) == 0
+        
+        
+       
