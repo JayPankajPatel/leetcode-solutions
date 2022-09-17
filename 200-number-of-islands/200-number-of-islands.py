c8c3371844d2d3d4ed2fb93969a5d9c3f92f051b
Jay Patel <37052409+JayPankajPatel@users.noreply.github.com>
2022-09-16 17:07:30 -0700

    Time: 449 ms (62.74%), Space: 16.5 MB (64.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,36 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_row = len(grid)
-        num_col = len(grid[0])
-        def get_neighbors(coor):
-            row, col = coor
-            res = []
-            delta_row = [0, 1, 0, -1]
-            delta_col = [-1, 0, 1, 0] 
-            n = len(delta_row)
-            for i in range(n):
-                neighbor_row = row + delta_row[i]
-                neighbor_col = col + delta_col[i]
-                if 0 <= neighbor_row < num_row and 0 <= neighbor_col < num_col:
-                    res.append((neighbor_row, neighbor_col))
-            return res
-        def bfs(root):
-            queue = deque([root])
-            while(len(queue)):
-                    node = queue.popleft()
-                    for neighbor in get_neighbors(node):
-                        row, col = neighbor
-                        if grid[row][col] == "0":
-                            continue
-                        queue.append(neighbor)
-                        grid[row][col] = "0"
-        count = 0
-        for _r in range(num_row):
-            for _c in range(num_col):
-                if grid[_r][_c] == "0":
-                    continue
-                else:
-                    bfs((_r,_c))
-                    count += 1
-        return count
+        if len(grid) == 0:
+            return 0
         
+        count = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    count += 1
+                    self.helper(grid, i, j)
+        
+        return count
+    
+    def helper(self, grid: List[List[str]], row: int, col: int) -> None:
+        while row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == '0':
+            return
+        
+        grid[row][col] = '0'
+        
+        self.helper(grid, row + 1, col)
+        self.helper(grid, row - 1, col)
+        self.helper(grid, row, col + 1)
+        self.helper(grid, row, col - 1)
